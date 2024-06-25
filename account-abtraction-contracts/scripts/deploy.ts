6d8af6ba29a04dfbbfc8181163608cecb4e9bd0b
Truong Ma Phi <trmaphi@gmail.com>
2024-06-25 17:47:51 +0700

    chore: update deploy script

--- account-abtraction-contracts/scripts/deploy.ts
+++ account-abtraction-contracts/scripts/deploy.ts
@@ -1,11 +1,18 @@
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
 import { ethers } from "hardhat";
 
 async function main() {
-  const lock = await ethers.deployContract("WhiteListSessionValidationModule", [unlockTime], {
-    value: lockedAmount,
+  const signer = new ethers.Wallet(process.env.PRIVATE_KEY as string, ethers.provider);
+  console.log("Deploying contracts with the account:", signer.address);
+  const whitelistSessionValidationModule = await ethers.deployContract("WhiteListSessionValidationModule", [
+    signer.address,
+    [],
+  ], {
+    signer: signer,
   });
 
-  await lock.waitForDeployment();
+  await whitelistSessionValidationModule.waitForDeployment();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
